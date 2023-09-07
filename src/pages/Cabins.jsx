57c1963faa0b9a1650a1fb5a36ac6ec30e1e8c17
Zhou Manze <manzezz1996@gmail.com>
2023-09-07 15:29:34 -0400

    applied react-hook-form to update cabin data both in the database and ui

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,13 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {getCabins} from "../services/apiCabins.js";
 import CabinTable from "../features/cabins/CabinTable.jsx";
+import CreateCabinForm from "../features/cabins/CreateCabinForm.jsx";
+import Button from "../ui/Button.jsx";
 
 function Cabins() {
+    const [showForm, setShowForm] = useState(false);
 
     useEffect((function () {
         getCabins().then(data => console.log(data))
@@ -18,9 +21,9 @@ function Cabins() {
             </Row>
 
             <Row>
-                <CabinTable>
-
-                </CabinTable>
+                <CabinTable />
+                <Button onClick={() => setShowForm(!showForm)}>{showForm ? 'Close' : 'Add new cabin'}</Button>
+                {showForm && <CreateCabinForm />}
             </Row>
         </>
     );
