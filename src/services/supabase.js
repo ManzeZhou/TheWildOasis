95913807f98e26d0425634b20066e6fa75df75aa
Zhou Manze <manzezz1996@gmail.com>
2023-09-13 16:19:20 -0400

    handled cabin create form errors and upload cabin images through form

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://dewfqnmhekdvwpnbwdts.supabase.co'
+export const supabaseUrl = 'https://dewfqnmhekdvwpnbwdts.supabase.co'
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRld2Zxbm1oZWtkdndwbmJ3ZHRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4NDIzMzUsImV4cCI6MjAwOTQxODMzNX0.ZZJKmYG-dOiFs66il8bFKd3y3jQ_UC5FDph8_9H4tqE"
 const supabase = createClient(supabaseUrl, supabaseKey);
 
